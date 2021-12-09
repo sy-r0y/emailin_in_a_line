0966da1795990a52df8154c9519eeb15d9d09f6a
Soumyajit Roy <roy.valmont@gmail.com>
2021-12-09 17:38:29 +0530

    updated emailer

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,5 +1,5 @@
 class EmailMailer < ActionMailer::Base
-  default from: "roy.0805@gmail.com"
+  default from: "youremail@emailprovider.com"  #  change in production
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
